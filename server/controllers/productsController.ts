e15cc0a0d591fde4854ac0669756b3aafa78013d
uros-simeunovic <dixypixy007@gmail.com>
2023-12-23 20:00:23 +0100

    Products endpoints

--- server/controllers/productsController.ts
+++ server/controllers/productsController.ts
@@ -1,24 +1,84 @@
 import { Request, Response } from "express";
+import Product from "../models/productModel"
 
-const getAllProducts = (req: Request, res: Response) => {
-    
-    res.status(200).send("Get All Products")
+const getAllProducts = async (req: Request, res: Response) => {
+    try {
+        const allProducts = await Product.find();
+
+        res.status(200).json({
+            status: "success",
+            data: allProducts
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: "error",
+            error: error
+        });
+    }
 }
 
-const getProductById = (req: Request, res: Response) => {
-    res.status(200).send("Get Product by Id")
+const getProductById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+
+        res.status(200).json({
+            status: "success",
+            data: product
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: "error",
+            error: error
+        });
+    }
 }
 
-const createProduct = (req: Request, res: Response) => {
-    res.status(200).send("Get All Products")
+const createProduct = async (req: Request, res: Response) => {
+    try {
+        const document = await Product.create(req.body);
+
+        res.status(201).json({
+            status: "success",
+            data: document
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: "error",
+            error: error
+        });
+    }
 }
 
 const updateProduct = (req: Request, res: Response) => {
     res.status(200).send("Get All Products")
 }
 
-const deleteProduct = (req: Request, res: Response) => {
-    res.status(200).send("Get All Products")
+const deleteProduct = async (req: Request, res: Response) => {
+     try {
+        const { id } = req.params;
+        const document = await Product.deleteOne({_id: id});
+
+        if(document.deletedCount == 0) {
+            res.status(404).json({
+                status: "Product not found"
+            })
+        }
+
+        res.status(201).json({
+            status: "success",
+            data: document
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: "error",
+            error: error
+        });
+    }
 }
 
 export default {
